d0ff41f745cb042a4ff46f0115f48e2ef8a24aab
ramandas <i.ramandas@gmail.com>
2021-12-16 22:38:52 +0530

    checks added in PR

--- brigade.js
+++ brigade.js
@@ -1,45 +1,51 @@
-const { events, Job, Group } = require('brigadier')
+const {events, Job, Group} = require("brigadier");
+const checkRunImage = "brigadecore/brigade-github-check-run:latest"
 
-// registering checks
-const project = {
-  name: "unit"
-}
+events.on("check_suite:requested", checkRequested)
+events.on("check_suite:rerequested", checkRequested)
+events.on("check_run:rerequested", checkRequested)
 
-events.on("push", (e, project) => {
-  console.log("Starting brigade")
-  console.log(project.name)
-
-  // creating a unit test application job
-  var unitTest = new Job("unit-test", "python:3")
-  unitTest.tasks = [
-    "cd /src/",
-    "pip install -r requirements.txt",
-    "cd /src/",
-    "python setup.py test"
-  ]
-  const dockerBuild = new Job("dockerbuild")
-  dockerBuild.image = "docker:dind"
-  dockerBuild.privileged = true;
-
-  dockerBuild.env = {
-    DOCKER_DRIVER: "overlay"
+function checkRequested(e, p) {
+  console.log("check requested")
+  // Common configuration
+  const env = {
+    CHECK_PAYLOAD: e.payload,
+    CHECK_NAME: "MyService",
+    CHECK_TITLE: "Echo Test",
   }
 
-  dockerBuild.env.DOCKER_USER = project.secrets.dockerLogin
-  dockerBuild.env.DOCKER_PASS = project.secrets.dockerPass
+  // This will represent our build job. For us, it's just an empty thinger.
+  const build = new Job("build", "alpine:3.7", ["sleep 60", "echo hello"])
 
-  dockerBuild.tasks = [
-    "dockerd-entrypoint.sh &",
-    "sleep 20",
-    "cd /src/",
-    "docker build -t ramandas/brigade-test:latest .",
-    "docker login -u $DOCKER_USER -p $DOCKER_PASS",
-    "docker push ramandas/brigade-test:latest"
-  ]
+  // For convenience, we'll create three jobs: one for each GitHub Check
+  // stage.
+  const start = new Job("start-run", checkRunImage)
+  start.imageForcePull = true
+  start.env = env
+  start.env.CHECK_SUMMARY = "Beginning test run"
 
-  const pipeline = new Group()
-  pipeline.add(unitTest)
-  //pipeline.add(dockerBuild)
-  pipeline.runAll()
+  const end = new Job("end-run", checkRunImage)
+  end.imageForcePull = true
+  end.env = env
 
-})
+  // Now we run the jobs in order:
+  // - Notify GitHub of start
+  // - Run the test
+  // - Notify GitHub of completion
+  //
+  // On error, we catch the error and notify GitHub of a failure.
+  start.run().then(() => {
+    return build.run()
+  }).then( (result) => {
+    end.env.CHECK_CONCLUSION = "success"
+    end.env.CHECK_SUMMARY = "Build completed"
+    end.env.CHECK_TEXT = result.toString()
+    return end.run()
+  }).catch( (err) => {
+    // In this case, we mark the ending failed.
+    end.env.CHECK_CONCLUSION = "failure"
+    end.env.CHECK_SUMMARY = "Build failed"
+    end.env.CHECK_TEXT = `Error: ${ err }`
+    return end.run()
+  })
+}
